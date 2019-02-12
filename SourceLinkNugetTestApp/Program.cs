163f19544f508a7a0283fa45837b8ae28db8d912
Ondrej Moravcik <ondrej.moravcik@monster.com>
2019-02-12 17:01:23 +0100

    Initial commit

---
+++ SourceLinkNugetTestApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SourceLinkNuget;
+using PrivateSourceLinkNuget;
+
+namespace SourceLinkNugetTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestNugetFromPublicRepo();
+            TestNugetFromPrivateRepo();
+        }
+
+        private static void TestNugetFromPrivateRepo()
+        {
+            var c = new PrivateClass1();
+            c.PrivateRepoTest();
+            try
+            {
+                c.PrivateRepoThrowException();
+            }
+            catch (Exception e)
+            {
+                var x = e.Data;
+            }
+        }
+
+        private static void TestNugetFromPublicRepo()
+        {
+            var c = new Class1();
+            c.Test();
+            try
+            {
+                c.ThrowException();
+            }
+            catch (Exception e)
+            {
+                var x = e.Data;
+            }
+        }
+
+    }
+}
